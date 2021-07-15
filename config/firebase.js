5ecd48030dcd448d74b0f0c8774018c9c6965deb
Hussain Rizvi <55734494+Hussain-7@users.noreply.github.com>
2021-07-15 16:59:12 +0500

    editted config

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,12 @@
 import firebase from "firebase";
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyAE5td6-Dd7jnkb7ZAaGOAlkHpPfSgHt2s",
+  authDomain: "whatsapp-clone-v2-38db2.firebaseapp.com",
+  projectId: "whatsapp-clone-v2-38db2",
+  storageBucket: "whatsapp-clone-v2-38db2.appspot.com",
+  messagingSenderId: "517726632830",
+  appId: "1:517726632830:web:1b324efc303378aa9a912f"
+};
 
 // Not intitializing firebase apps if already exists
 const app = !firebase.apps.length
